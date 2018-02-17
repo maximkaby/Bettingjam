bf47136f5f0d16d3b4bdf3aad40b14a7a87d57ba
ArteezyCode <arteezy19.code@gmail.com>
2018-02-17 13:31:01 +0300

    icons / manifest /link edited

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,7 +6,7 @@ export default class Footer extends Component {
       <div className="footer">
         <div className="footer__title">
           <span className="title__powered">Powered by</span>
-          <a href="https://www.bettingjam.co.uk/">
+          <a href="https://www.bettingjam.co.uk/" target="_blank">
             <span className="title__betting">betting</span>
             <span className="title__jam">jam</span>
           </a>
